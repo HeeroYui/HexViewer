6b39a7f6953162ffc6a91004b198493939a7a165
Edouard DUPIN <yui.heero@gmail.com>
2015-04-12 09:04:25 +0200

    [DEV] build error

--- generalDefine.h
+++ generalDefine.h
@@ -34,7 +34,7 @@ typedef   signed short int              int16_t;
 typedef unsigned long int               uint32_t;
 //typedef   signed long int               int32_t;
 typedef unsigned long long int          uint64_t;
-typedef   signed long long int          int64_t;
+//typedef   signed long long int          int64_t;
 //typedef unsigned long long long int     uint128_t;
 //typedef   signed long long long int     int128_t;
 
